dae4548936905a055972dc8f88b5b2d3e2217028
superdee <marcin.perdeusz@gmail.com>
2022-06-19 13:50:22 +0200

    OLX scraper updated - website's structure change

--- scrapers.py
+++ scrapers.py
@@ -11,7 +11,7 @@ from concurrent.futures import ThreadPoolExecutor
 
 class Scraper(ABC):
 
-    def __init__(self, keywords:Set, conditions:Dict, excluded:Set):
+    def __init__(self, keywords: Set, conditions: Dict, excluded: Set):
         self.keywords = keywords
         self.conditions = conditions
         self.excluded_words = excluded
@@ -55,12 +55,84 @@ class Scraper(ABC):
 
 
 class ScraperOLX(Scraper):
+    URLS = {'base': 'https://www.olx.pl/d/oferty/q-',
+            'inp_text':    '/',
+            'price_from':  '&search[filter_float_price%3Afrom]=',
+            'price_to':    '&search[filter_float_price%3Ato]=',
+            'page':      '&page='}
+
+    @staticmethod
+    def offer_info(offer_tag) -> Offer:
+        """Get offer info from offer tag"""
+
+        offer_title = offer_tag.find('h6').text
+        offer_link = offer_tag["href"]
+        offer_id = hash(offer_link)
+
+        offer_price_text = offer_tag.find("p", attrs={"data-testid": "ad-price"}).text.strip().replace(' ', '')
+        offer_price = int(re.findall(r'\d+', offer_price_text)[0])
+
+        return Offer(offer_id, offer_title, offer_price, offer_link, datetime.utcnow())
+
+    @staticmethod
+    def check_offers_count(page_url: str) -> int:
+        """Tries to load the page and extract the number of offers available"""
+
+        try:
+            page = requests.get(page_url)
+        except Exception as err:
+            print("GET request fail:", err)
+            return -1
+
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+        try:
+            offers_tag = soup.find('div', attrs={"data-testid": "total-count"}).text
+            # offers_tag = soup.find('div', attrs={"data-cy": "search_results_info_results_count"}).find('h2').text
+            offers_count = int(re.findall(r'\d+', offers_tag.strip().replace(' ', ''))[0])
+        except AttributeError as err:
+            print("No offers found")
+            return -1
+
+        return offers_count
+
+    def generate_urls(self) -> List[str]:
+        """"Method for generating list of urls from the keywords and conditions"""
+
+        conditions_url = '/?' + ''.join({self.URLS[con_name] + str(con_val) for (con_name, con_val) in self.conditions.items()})
+        return [self.URLS['base'] + keyword.replace(' ', '-') + conditions_url for keyword in self.keywords]
+
+    def list_offers(self, page_url: str, offers_count: int) -> Set[Offer]:
+        """Iterates over offers page and parses them into a set"""
+
+        offers = set()
+        page_num = 1
+
+        while len(offers) < offers_count:
+            cur_page_url = page_url + self.URLS['page'] + str(page_num)
+            page = requests.get(cur_page_url)
+
+            if self.URLS["base"] not in page.url:
+                continue
+
+            content_soup = BeautifulSoup(page.text, 'html.parser')
+            offers_tags = content_soup.find('div', attrs={"data-testid": "listing-grid"}).find_all('a')
+
+            for offer_tag in offers_tags:
+                offer = self.offer_info(offer_tag)
+                offers.add(offer)
+
+            page_num += 1
+
+        return offers
+
+
+class ScraperOldOLX(Scraper):
     URLS = {'base': 'https://www.olx.pl/oferty/q-',
             'inp_text':    '/',
             'price_from':  '&search[filter_float_price%3Afrom]=',
             'price_to':    '&search[filter_float_price%3Ato]=',
             'page':      '&page='}
-    OFFERS_PER_PAGE = 30
 
     @staticmethod
     def offer_info(offer_tag) -> Offer:
@@ -100,6 +172,10 @@ class ScraperOLX(Scraper):
     def generate_urls(self) -> List[str]:
         """"Method for generating list of urls from the keywords and conditions"""
 
+        # base_url = self.URLS['base'] + self.URLS['inp_text']
+        # conditions_url = ''.join({self.URLS[con_name] + str(con_val) for (con_name, con_val) in self.conditions.items()})
+        # return [base_url + urllib.parse.quote_plus(keyword) + conditions_url for keyword in self.keywords]
+
         conditions_url = '/?' + ''.join({self.URLS[con_name] + str(con_val) for (con_name, con_val) in self.conditions.items()})
         return [self.URLS['base'] + keyword.replace(' ', '-') + conditions_url for keyword in self.keywords]
 
@@ -109,17 +185,14 @@ class ScraperOLX(Scraper):
         offers = set()
         page_num = 0
 
-        cur_page_url = page_url + self.URLS['page'] + str(page_num)
-        page = requests.get(cur_page_url)
-
         while len(offers) < offers_count:
             cur_page_url = page_url + self.URLS['page'] + str(page_num)
+            page = requests.get(cur_page_url)
 
-            if cur_page_url not in page.url:
-                page = requests.get(cur_page_url)
+            if self.URLS["base"] not in page.url:
+                continue
 
             content_soup = BeautifulSoup(page.text, 'html.parser')
-            # offers_tags = content_soup.find(id='offers_table').find_all('div', class_='offer-wrapper')
             offers_tags = content_soup.find(id='offers_table').find_all('tr', class_='wrap')
 
             for offer_tag in offers_tags:
